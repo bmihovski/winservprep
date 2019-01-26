0bfba4b9fa78518e673ae35bfcbd024bf75cbb43
bmihovski <bmihovski@live.com>
2019-01-26 08:15:55 -0800

    First step completed

---
+++ AD.ps1
@@ -0,0 +1,12 @@
+﻿#1. Create an infrastructure with two Windows Server machines:
+#    ◦ M1 with two network adapters – one connected to the external LAN and one connected to an internal (private) network
+#    ◦ M2 with one network adapter connected to the internal (private) network
+Get-NetAdapter
+Rename-NetAdapter -Name ethernet -NewName Internet
+Rename-NetAdapter -Name 'ethernet 2' -NewName Internal
+Rename-Computer -NewName DC1
+Restart-Computer
+# CSERVER1
+Rename-NetAdapter -Name ethernet -NewName internal
+Rename-Computer -NewName CSERVER1
+Restart-Computer
