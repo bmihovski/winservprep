5238541c107523e6e89802cb10a7ab32abd9b929
bmihovski <bmihovski@live.com>
2019-01-26 12:47:32 -0800

    dhcp server setup ad

--- AD.ps1
+++ AD.ps1
@@ -11,3 +11,14 @@ Restart-Computer
 Rename-NetAdapter -Name ethernet -NewName internal
 Rename-Computer -NewName M2
 Restart-Computer
+#2. Install and configure DHCP role on M1 with binding to the internal network adapter and scope of 192.168.100.0/24
+New-NetIPAddress -IPAddress 192.168.100.1 -PrefixLength 24 -InterfaceAlias internal
+Install-WindowsFeature -Name DHCP -IncludeManagementTools
+Add-DhcpServerSecurityGroup -computername M1
+Restart-Service -Name DHCPServer
+Get-DhcpServerv4Binding
+Set-DhcpServerv4Binding -InterfaceAlias internal -BindingState $true
+Add-DhcpServerv4Scope -Name 'DHCP Internal Network' -StartRange 192.168.100.100 -EndRange 192.168.100.150 -SubnetMask 255.255.255.0
+Get-DhcpServerv4Binding
+Set-DhcpServerv4OptionValue -OptionId 3 -Value 192.168.100.1
+Set-DhcpServerv4OptionValue -OptionId 6 -Value 192.168.100.1 -Force
